203c4e854c061a55d08ea02379bab312797bb006
Kamil Ziętek <qamil1995@gmail.com>
2020-04-06 17:55:12 +0200

    Removed 2.0 from name, added Chapter and Song classes

---
+++ MainMasterGenerator/Chapter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SonglistGenerator
+{
+    class Chapter
+    {
+        /// <summary>
+        /// Defines whether \Zespoltrue and \Zespolfalse sections are added to master.tex
+        /// </summary>
+        bool UseArtists { get; }
+
+        string FolderName { get; }
+
+        string ChapterName { get; }
+
+        List<Song> Songs { get; }
+    }
+}
---
+++ MainMasterGenerator/Song.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace SonglistGenerator
+{
+    /// <summary>
+    /// Representation of each song_title.tex file
+    /// </summary>
+    class Song
+    {
+        /// <summary>
+        /// Filename with subfolder if exists, this string is used in master.tex file
+        /// </summary>
+        string FilePath { get; }
+
+        /// <summary>
+        /// Song title, first {} in \tytul section.
+        /// </summary>
+        string Title { get; }
+
+        /// <summary>
+        /// Song author (text, music), second {} in \tytul section.
+        /// </summary>
+        string Author { get; }
+
+        /// <summary>
+        /// Song artist, third {} in \tytul section.
+        /// Additionally used in "Rozne" directory, to allow multiple artists in one category.
+        /// Visible if "\Zespoltrue" set in chapter master.tex file, otherwise ignored.
+        /// </summary>
+        string Artist { get; }
+
+        List<string> Text { get; }
+
+        List<string> Chords { get; }
+    }
+}
