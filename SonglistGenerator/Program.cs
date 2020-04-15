f1f2eb2f73b6a72df146e4aded0cd3fdbd42aeb0
Kamil Ziętek <qamil1995@gmail.com>
2020-04-15 17:57:59 +0200

    Added logger

--- SonglistGenerator/Program.cs
+++ SonglistGenerator/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using System.IO;
 
 namespace SonglistGenerator
 {
     class Program
     {
+        const string chapterMasterFile = "master.tex";
+        const string songbookMainFile = "main.tex";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var logger = new Logger();
+            logger.WriteLine("Hello World!");
+
+            var inputPath = args[0];
+            var listOfFolders = Directory.GetDirectories(inputPath);
+            foreach (var folder in listOfFolders)
+            {
+                if (!File.Exists(Path.Combine(folder, chapterMasterFile)))
+                {
+                    logger.WriteLine($"Folder {folder} does not cotain {chapterMasterFile}, ignoring.");
+                    continue;
+                }
+
+
+            }
         }
     }
 }
