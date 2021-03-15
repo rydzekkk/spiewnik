a8861b239e1f1f932112d44e1fc3c803d7d6a1d8
Kamil Ziętek <qamil1995@gmail.com>
2021-03-15 18:07:01 +0100

    Added initial version of song choser

---
+++ SongChooser/DisplaySong.cs
@@ -0,0 +1,22 @@
+﻿using SonglistGenerator;
+
+namespace SongChooser
+{
+    class DisplaySong
+    {
+        private Song song;
+
+        public DisplaySong(Song song, string chapter)
+        {
+            this.song = song;
+            this.Chapter = chapter;
+            this.Print = true;
+        }
+        public string Chapter { get; }
+        public bool Print { get; set; }
+        public string Title { get => song.Title; }
+        public string Author { get => song.Author; }
+        public string Artist { get => song.Artist; }
+        public string Path { get => song.Path; }
+    }
+}
---
+++ SongChooser/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using SonglistGenerator;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace SongChooser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Logger logger;
+        private Songlist songlist;
+        private List<DisplaySong> displayedSongs;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.logger = new Logger();
+            this.songlist = new Songlist(logger);
+            this.displayedSongs = new List<DisplaySong>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.dataGrid.ItemsSource = null;
+            var folders = Directory.GetDirectories(folderPath.Text);
+            songlist.CreateListOfChapters(folders);
+            songlist.CreateListOfSongs();
+            songlist.Initialize();
+
+            foreach (var chapter in songlist.OrderedChapters)
+            {
+                foreach (var song in chapter.Songs)
+                {
+                    this.displayedSongs.Add(new DisplaySong(song, chapter.ChapterName));
+                }
+            }
+
+            this.dataGrid.ItemsSource = this.displayedSongs;
+        }
+    }
+}
